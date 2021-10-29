e2dbe8ae1646b2f29ece60744028f91687023de0
Seong Kyu Sohn <nullterra180@gmail.com>
2021-10-29 14:32:31 +0900

    chore: add example animation code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,35 +5,74 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-}
-
-// fading button
-class ButtonFade extends StatefulWidget {
-  @override
-  createState() => ButtonFadeState();
-}
-
-class ButtonFadeState extends State<ButtonFade> {
-  double opacityLevel = 1.0;
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        AnimatedOpacity(
-          opacity: opacityLevel,
-          duration: Duration(seconds: 2),
-          child: ElevatedButton(
-            child: Text('fade me'),
-            onPressed: () {
-              setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
-            },
-          ),
-        ),
-      ],
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
     );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition({required this.child, required this.animation, Key? key})
+      : super(key: key);
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GrowTransition(
+      child: const LogoWidget(),
+      animation: animation,
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
